9429190cfad4dfc349b9848062c6a8cc03828da9
Ashif Hassan <ashifhassan1996@gmail.com>
2024-11-11 23:02:49 +0530

    Add controller files

---
+++ controllers/indexController.js
@@ -0,0 +1,38 @@
+const getHome = (req, res) => {
+    const locals = { title: "Home | SnapURL!" };
+    return res.render("index", {
+        locals,
+        layout: "layout/mainLayout",
+    });
+};
+
+const getAbout = (req, res) => {
+    const locals = { title: "About Us | SnapURL!" };
+    return res.render("about", {
+        locals,
+        layout: "layout/mainLayout",
+    });
+};
+
+const getContact = (req, res) => {
+    const locals = { title: "Contact Us | SnapURL!" };
+    return res.render("contact", {
+        locals,
+        layout: "layout/mainLayout",
+    });
+};
+
+const getShortenedUrl = (req, res) => {
+    const locals = { title: "Shortened Url | SnapURL!" };
+    return res.render("result", {
+        locals,
+        layout: "layout/mainLayout",
+    });
+};
+
+module.exports ={
+    getHome,
+    getAbout,
+    getContact,
+    getShortenedUrl,
+};
