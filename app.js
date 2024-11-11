cf6499a09e74d89c6c1592adfeaebf0f4152b781
Ashif Hassan <ashifhassan1996@gmail.com>
2024-11-11 22:55:22 +0530

    New dependencies installed

--- app.js
+++ app.js
@@ -1,14 +1,48 @@
 require('dotenv').config();
 
 const express = require('express');
+const path = require('path');
+const nocache = require('nocache');
+const expressLayouts = require('express-ejs-layouts');
+const flash = require('connect-flash');
+
 const app = express();
 
-const PORT = process.env.PORT || 8000;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(flash());
 
-app.get('/', (req, res) => {
-    res.send("Hello World!!!");
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(expressLayouts);
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(nocache());
+
+const indexRoutes = require('./routes/indexRoutes');
+app.use('/', indexRoutes);
+
+app.use((req, res, next) => {
+    const locals = { title: "404 | Page Not Found" };
+    return res.status(404).render("404", {
+        locals,
+        layout: "layout/errorLayout",
+    });
 });
 
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+
+    const locals = { title: "500 | Internal Server Error" };
+    return res.status(500).render('internalError', {
+        locals,
+        layout: 'layout/errorLayout',
+    });
+});
+
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}...`);
 });
+
+module.exports = app;
