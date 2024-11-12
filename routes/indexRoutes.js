da2b6f49cbf9ac6106d1a7508851e76d453a1721
Ashif Hassan <ashifhassan1996@gmail.com>
2024-11-12 15:35:42 +0530

    Add shortenUrl route

--- routes/indexRoutes.js
+++ routes/indexRoutes.js
@@ -6,6 +6,8 @@ const indexController = require('../controllers/indexController');
 router.get('/', indexController.getHome);
 router.get('/about', indexController.getAbout);
 router.get('/contact', indexController.getContact);
-router.get('/shortened-url', indexController.getShortenedUrl);
+
+router.post('/shorten-url', indexController.shortenUrl);
+router.get('//shortened-url', indexController.getShortenedUrl);
 
 module.exports = router;
