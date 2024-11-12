b59e5ea9c338cf149a9d23e8c28415c844914795
Ashif Hassan <ashifhassan1996@gmail.com>
2024-11-12 15:33:44 +0530

    Add shortenUrl controller function

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,4 +1,5 @@
 const Url = require('../models/urlModel');
+const generateShortenedUrl = require('../utils/urlShortener.js');
 
 const getHome = (req, res) => {
     const locals = { title: "Home | SnapURL!" };
@@ -7,10 +8,6 @@ const getHome = (req, res) => {
         layout: "layout/mainLayout",
     });
 };
-
-const shortenUrl = async (req, res) => {
-
-}
 
 const getAbout = (req, res) => {
     const locals = { title: "About Us | SnapURL!" };
@@ -28,6 +25,44 @@ const getContact = (req, res) => {
     });
 };
 
+const shortenUrl = async (req, res) => {
+    const { originalUrl } = req.body;
+
+    try {
+        const existingUrl = await Url.findOne({ originalUrl });
+        if (existingUrl) {
+            return res.status(200).json({
+                success: true,
+                message: 'The URL you provided is already shortened. Click OK button to see the shortened URL.',
+                shortenedUrl: existingUrl.shortenedUrl,
+            });
+        }
+
+        const shortenedUrl = await generateShortenedUrl(req);
+
+        const newShortenedUrl = new Url({
+            originalUrl,
+            shortenedUrl,
+            createdAt: new Date(),
+            expiresAt: null,
+        });
+
+        await newShortenedUrl.save();
+
+        return res.status(201).json({
+            success: true,
+            message: 'URL successfully shortened. Click OK button to see the shortened URL.',
+            shortenedUrl,
+        });
+    } catch (error) {
+        console.error('Error shortening the URL:', error);
+        return res.status(500).json({
+            success: false,
+            message: 'Internal server error. Please try again later.',
+        });
+    }
+};
+
 const getShortenedUrl = (req, res) => {
     const locals = { title: "Shortened Url | SnapURL!" };
     return res.render("result", {
@@ -38,6 +73,7 @@ const getShortenedUrl = (req, res) => {
 
 module.exports ={
     getHome,
+    shortenUrl,
     getAbout,
     getContact,
     getShortenedUrl,
